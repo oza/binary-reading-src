8b1f645b76d637f808108a0cc05d21cc42494beb
Tsuyoshi Ozawa <ozawa@t-oza.net>
2009-09-26 07:44:23 +0900

    add dummy mehod and header

--- Makefile
+++ Makefile
@@ -2,13 +2,21 @@ GCC=gcc
 MAKE=make
 TARGET=elfparser
 
-.PHONY : all clean
+.PHONY : all test run clean 
 
 all:
-	$(MAKE) elfparser
+	$(MAKE) $(TARGET)
+	$(MAKE) test
 
 $(TARGET): $(TARGET).c
 	$(GCC) $< -o $@
+
+run:
+	./$(TARGET)
+
+test:
+	@echo "start to test..."
+	@echo "do something..."
 
 clean:
 	rm -rf *.o $(TARGET)
--- elfparser.c
+++ elfparser.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include "elfparser.h"
 
+#define MAX_BUFFER_SIZE 4096
+
+bool is_elf(byte buf[]){
+	return true;
+}
 
 int main(int argc,char *argv[])
 {
+	byte buf[MAX_BUFFER_SIZE];
 
+	if ( is_elf(buf) ){
+		puts("This file is elf file!");
+	}else {
+		puts("This file is not elf file! Stop to parse.");
+		exit(1);
+	}
 
+	exit(0);
 }
